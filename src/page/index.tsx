4252055b46c55133c7f37d6344d14904d3d80e93
Russell Lin <russell.lin@wordup.com.tw>
2022-09-04 15:02:03 +0800

    fulfill basic settings for TypeScript, tailWindCSS

---
+++ src/page/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoList: React.FC = () => {
+  return (
+    <div>
+      <div className="text-3xl">test</div>
+    </div>
+  );
+};
+
+export default TodoList;
